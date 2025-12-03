de2569d97888f0e049587661c8f8c9191b28cfa7
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2025-12-03 23:18:04 +0100

    fix: rename parameter in BshPostInterceptor for clarity

--- src/client/types.ts
+++ src/client/types.ts
@@ -40,6 +40,6 @@ export type BshAuthFn = () => Promise<AuthToken | undefined | null>;
 
 export type BshRefreshTokenFn = () => Promise<string | undefined | null>;
 
-export type BshPostInterceptor<T = any> = (result: BshResponse<T>, params?: BshClientFnParams<T>) => Promise<BshResponse<T>>;
+export type BshPostInterceptor<T = any> = (response: BshResponse<T>, params?: BshClientFnParams<T>) => Promise<BshResponse<T>>;
 export type BshPreInterceptor<T = any, R = T> = (params: BshClientFnParams<T, R>) => Promise<BshClientFnParams<T, R>>;
 export type BshErrorInterceptor<T = any> = (error: BshError, response?: BshResponse<T>, params?: BshClientFnParams<T>) => Promise<BshError | undefined>;
